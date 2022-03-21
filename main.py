64ca20429b1652379ab5a73a25fb99bdfeadba04
Guilherme Cabral <100802844+gcab156@users.noreply.github.com>
2022-03-21 20:59:40 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 import json
 
 
+os.system("sudo service tor start")
 
 def function():
     global function
@@ -11,61 +12,43 @@ def function():
     is_on = True
     man_on = PhotoImage(file="src/man_on.png")
     man_off = PhotoImage(file="src/man_off.png")
-    
-    network_down = 'ifconfig wlan0 down && ifconfig eth0 down'
-    os.system(network_down)
-    
-    change_mac = 'macchanger wlan0 --random && macchanger eth0 --random'
-    os.system(change_mac)
-    
-    network_up = 'ifconfig wlan0 up && ifconfig eth0 up'
-    os.system(network_up)
         
-    start = 'service tor start && anonsurf start'
+    start = 'anonsurf start'
     os.system(start)
 
     if is_on:
         Label(root, text="", bg="white", image=man_on).place(x="-13", y="-26")
-        Button(text="Disconnect", bg="white", fg="Black", command=disconnect, width=13, height=1).place(x=90, y=279)
+        Button(text="Disconnect", bg="white", fg="Black", command=disconnect, width=13, height=1).place(x=75, y=279)
+        Button(text="Change IP", bg="white", fg="Black", command=change, width=13, height=1).place(x=75, y=309)
         is_on = False
     else:
         Label(root, text="", bg="white", image=man_off).place(x="-13", y="-26")
-        Button(text="Connect", bg="white", fg="Black", command=function, width=13, height=1).place(x=90, y=279)
+        Button(text="Connect", bg="white", fg="Black", command=function, width=13, height=1).place(x=75, y=279)
         is_on = True
-    
-    request = requests.get('https://ipinfo.io/json')
-    country = request.json()['country']
-    region = request.json()['region']
-    
-    Label(root, text=f"{region}, {country}", bg="white", fg="black").place(x=57, y=406)
-    Button(text="Change IP", bg="white", fg="Black", command=change, width=13, height=1).place(x=90, y=309)
+        
+    os.system("anonsurf restart")    
         
     root.mainloop()
     function()
-        
-    
-   
+
+
 def disconnect():
     stop = 'anonsurf stop && service tor stop'
     os.system(stop)
     
     man_off = PhotoImage(file="src/man_off.png")
     Label(root, text="", bg="white", image=man_off).place(x="-13", y="-26")
-    Button(text="Connect", bg="white", fg="Black", command=function, width=13, height=1).place(x=90, y=279)
-    
-    request = requests.get('https://ipinfo.io/json')
-    country = request.json()['country']
-    region = request.json()['region']
-    
-    Label(root, text=f"{region}, {country}", bg="white", fg="black").place(x=57, y=406)
-    Button(text="Change IP", bg="white", fg="Black", command=change, width=13, height=1, state="disabled").place(x=90, y=309)
+    Button(text="Connect", bg="white", fg="Black", command=function, width=13, height=1).place(x=75, y=279)
+
+    Label(root, text="Unknown", bg="white", fg="black").place(x=57, y=406)
+    Button(text="Change IP", bg="white", fg="Black", command=change, width=13, height=1, state="disabled").place(x=75, y=309)
     
     root.mainloop()
     disconnect()
     
     
 def change():
-    change = 'anonsurf change'  
+    change = 'anonsurf changeid'  
     os.system(change)
 
 
@@ -76,17 +59,13 @@ def screen():
     root.title("AnonVPN")
     root.configure(background="White")
 
-    Button(text="Connect", bg="white", fg="Black", command=function, width=13, height=1).place(x=90, y=279)
-    Button(text="Change IP", bg="white", fg="Black", command=change, width=13, height=1, state="disabled").place(x=90, y=309)
-    
-    request = requests.get('https://ipinfo.io/json')
-    country = request.json()['country']
-    region = request.json()['region']
+    Button(text="Connect", bg="white", fg="Black", command=function, width=13, height=1).place(x=75, y=279)
+    Button(text="Change IP", bg="white", fg="Black", command=change, width=13, height=1, state="disabled").place(x=75, y=309)
   
     man_off = PhotoImage(file="src/man_off.png")
     Label(root, text="", bg="white", image=man_off).place(x="-13", y="-26")
   
-    Label(root, text=f"{region}, {country}", bg="white", fg="black").place(x=57, y=406)
+    Label(root, text="Unknown", bg="white", fg="black").place(x=57, y=406)
     
     world = PhotoImage(file="src/world.png")
     Label(root, text="", bg="white", image=world).place(x=10, y=397)
